8fdb081ad54f4af4ece41d84677c040894c46727
Анна Новикова <theworldisgonnarollme03@gmail.com>
2025-04-18 13:03:40 +0300

    update report making and add score

--- analyze.py
+++ analyze.py
@@ -6,6 +6,14 @@ from docx2pdf import convert
 import datetime as dt
 import json
 
+WEIGHTS = {
+    "Security": 3.0,
+    "CodeStyle": 1.5,
+    "CodeSmells": 2.0,
+    "AntiPatterns": 2.5,
+    "LegacyCompatibility": 1.0
+}
+
 def make_prompt(code_text: str) -> str:
     """"This function makes prompt for analyzing code
         code_text: text of the code to analyze
@@ -23,43 +31,47 @@ def make_prompt(code_text: str) -> str:
 
         Criterias:
         1. Code Smells – repetitive code, long methods, redundant conditions, poor organization.
-        2. Anti-Patterns – presence of architectural and design anti-patterns (e.g. God Object, Spaghetti Code, etc.)
+            If there's no smells, rate it as 10. The more smells the lower the score.
+        2. Anti-Patterns – presence of architectural and design anti-patterns (e.g. God Object, Spaghetti Code, etc.). 
+            If there's no anti-patterns, rate it as 10. The more anti-patterns the lower the score.
         3. Legacy Compatibility – if the code is old, evaluate how adequate the solutions are in the context of their time and 
             environment constraints.
+            If there's no legacy code, rate is as 10. The more legacy code the lower the score.
 
+        Make sure to use double quotes in JSON!!!! You don't need to repeat the text in the example!
         Output must look like this:
 
         json
         {
-            'Code Smells': {
-                'score': 6,
-                'comment': 'Repetetive code',
-                'examples': ['line 40-45'],
-                'legacy_context': false,
-                'forced_solution': false
+            "CodeSmells": {
+                "score": 6,
+                "comment": "Repetetive code",
+                "examples": "line 40-45, line 60-70",
+                "legacy_context": false,
+                "forced_solution": false
             },
             
-            'Anti Patterns': {
+            "AntiPatterns": {
                 "score": 4,
                 "comment": "Singleton applied in wrong place",
-                "examples": ["line 70"],
+                "examples": "line 80",
                 "legacy_context": true,
                 "forced_solution": true
             },
 
-            'Legacy Compatibility': {
-                'score': 10,
-                'comment': "The solutions are adequate for their time",
-                'examples': [],
-                'legacy_context': false,
-                'forced_solution': false
+            "LegacyCompatibility": {
+                "score": 10,
+                "comment": "The solutions are adequate for their time",
+                "examples": "",
+                "legacy_context": false,
+                "forced_solution": false
             }
         }
         
         The code to analyze is:
         """ + code_text
 
-def mistral_analyze(prompt: str, history: str = "") -> str:
+def mistral_analyze(prompt: str, history: str = "") -> dict:
     """This function analyzes code text on having anti patterns and code smells using mistral
         code: code to analyze
         history: history of previous responses
@@ -82,40 +94,58 @@ def mistral_analyze(prompt: str, history: str = "") -> str:
         "stream": False
     })
 
-    return response.json()['response']
+    try: 
+        result = json.loads(response.json()['response'], cls=LazyDecoder)
+    except Exception:
+        result = response.json()['response']
 
-def analyze_code_files(files: list[str]) -> str:
-    """This function analyzes files on having anti patterns and code smells using mistral
-    and gives feedback about them and how good the developer is
-        files: list of files with code to analyze
+    return result
+
+
+def analyze_diff_and_write_in_docx(diff_path: str, docx: str, history_file: str):
+    """This function analyzes diff file and gives feedback about it and how good the developer is
+        diff_path: path to diff file
+        docx: path to docx file
+        history_file: path to history files
     """
-    time = dt.datetime.now().strftime("%Y-%m-%d_%H-%M")
+    # сумма очков за каждый критерий * вес критерия
+    score = 0
 
-    history_file = f'history-{time}.txt'
-    report = Document()
-    report.add_heading('Отчет об оценке качества кода', 0)
-    p = report.add_paragraph()
-    p.add_run('Общая информация').bold = True
-    p = report.add_paragraph()
-    p.add_run('ФИО: ').bold = True
-    p.add_run('Котейкин Мяу Мяус')
+    # создаем отдельную папку для диффа, называем пока что по времени, чтобы при анализе других диффов не путаться
+    time = dt.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    output_dir = f'./restored/{time}'
 
-    report.add_heading('Выявленные проблемы', level=1)
+    # парсим диффы в файлы с кодом с сохраннием структуры папок
+    parse_diff_to_code_files(diff_path, output_dir)
+    diff_files = get_files_in_dir(output_dir)
 
-    # проходимся по каждому файлу и анализируем его, сохраняя историю диалога в файл
-    for file in files:
+    # ЗДЕСЬ БУДЕТ ДОБАВЛЕНИЕ ОГЛАВЛЕНИЯ С НАЗВАНИЕМ MR ИЛИ ТИПА ТОГО
+
+    for file in diff_files:
         code = read_file(file)
         prompt = make_prompt(code)
-        result = mistral_analyze(prompt)
+        ai_result = mistral_analyze(prompt)
+        # ЗДЕСЬ БУДЕТ СТАТИЧЕСКИЙ АНАЛИЗ
 
-        # переводи строку в json формат и обязательно используем декодером для избежания ошибок в работе программы
-        json_result = json.loads(result, cls=LazyDecoder)
-        write_file(history_file, f"User: {prompt}\nAssistant: {result}\n")
-        
-        p = report.add_paragraph()
-        p.add_run(f"Файл: {file}\n").bold = True
-        write_json_to_docx_file(report, json_result)
+        # записываем результат в файл истории
+        write_file(history_file, f"User: {prompt}\nAssistant: {ai_result}\n")
+        # ЗДЕСЬ БУДЕТ ЗАПИСЬ СТАТ АНАЛИЗА В ФАЙЛ ИСТОРИИ
 
+        p = docx.add_paragraph()
+        p.add_run(f"\nФАЙЛ: {file}").bold = True
+        write_json_to_docx_file(docx, ai_result)
+
+        for key in ai_result.keys():
+            small_json = ai_result[key]
+            score += small_json['score'] * WEIGHTS[key]
+
+        # ЗДЕСЬ БУДЕТ ЗАПИСЬ СТАТ АНАЛИЗА В ФАЙЛ docx
+        # ЗДЕСЬ БУДЕТ ДОБАВЛЕНИЕ ОЧКОВ ЗА СТАТ АНАЛИЗ В SCORE
+
+    # возвращаем словарь {кол-во файлов: общая оценка} для формирования оценки разработчика
+    return {len(diff_files): score}
+
+def make_review_and_write_in_docx(docx: str, history_file: str) -> None:
     review_prompt = """
         You are an experienced team leader and code reviewer. Your job is to give the developer feedback on the quality of their code.
         Analyze the code for the following aspects:
@@ -124,11 +154,12 @@ def analyze_code_files(files: list[str]) -> str:
         3. Give 2-3 specific tips on what the developer should learn or improve to write even better
         4. If there are positive or negative aspects associated with the outdated stack, be sure to mention it.
 
+        Make sure to use double quotes in JSON!!!! You don't need to repeat the text in the example!
         Return the answer in a strictly structured JSON format:
 
         json
         {
-            'Overall Review': {
+            "Overall Review": {
                 "strengths": "The code is well structured, modules are logically separated. 
                                 Explicit typing is used",
                 "improvements": "In places the code style is not followed - indents and line lengths. 
@@ -142,18 +173,10 @@ def analyze_code_files(files: list[str]) -> str:
         Do not include 'legacy_context' and 'forced_solution' into the final response's JSON!!!
         """
     history = read_file(history_file)
+    result = mistral_analyze(review_prompt, history)
+    
+    write_json_to_docx_file(docx, result)
 
-    # получаем анализ качества разработчика с учетом истории
-    final_result = mistral_analyze(review_prompt, history)
-    json_final_result = json.loads(final_result, cls=LazyDecoder)
-    write_json_to_docx_file(report, json_final_result)
 
-    report_name = f'report-{time}'
-    report.save(report_name + '.docx')
-    convert(report_name + '.docx', report_name + '.pdf')
 
-    delete_file(history_file)
-    delete_file(report_name + '.docx')
-
-    return f'Отчет сгенерирован в файле {report_name + '.pdf'}'
 
